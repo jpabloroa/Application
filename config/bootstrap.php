da28c310d3183165d4960b80da3a80059409f8eb
jpabloroa <jpablonano@gmail.com>
2021-12-07 14:37:00 -0500

    Correcci├│n de errores

--- config/bootstrap.php
+++ config/bootstrap.php
@@ -4,9 +4,13 @@ define("PROJECT_ROOT_PATH", __DIR__ . "/../");
  
 // include main configuration file
 require_once PROJECT_ROOT_PATH . "/config/config.php";
+require_once PROJECT_ROOT_PATH . "/config/index.php";
  
 // include the base controller file
 require_once PROJECT_ROOT_PATH . "/controller/api/BaseController.php";
+require_once PROJECT_ROOT_PATH . "/controller/index.php";
+require_once PROJECT_ROOT_PATH . "/controller/api/index.php";
  
 // include the use model file
 require_once PROJECT_ROOT_PATH . "/model/UserModel.php";
+require_once PROJECT_ROOT_PATH . "/model/index.php";
--- config/index.php
+++ config/index.php
@@ -1,7 +1,4 @@
 <?php
 
-//$urlPath = 
-require __DIR__ . "/config/bootstrap.php";
-
-header("location: " . $_SERVER["SERVER_NAME"] . "/proyectos/Application/config/");
+header("location: " . PROJECT_ROOT_PATH . "/nuevo.php");
 exit;
