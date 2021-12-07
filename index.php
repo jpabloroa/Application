29c7cef1b559582a00f2a3190802639ef582d64d
jpabloroa <jpablonano@gmail.com>
2021-12-07 18:52:57 -0500

    Correcci├│n de errores

--- index.php
+++ index.php
@@ -9,13 +9,27 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <script src="https://unpkg.com/html5-qrcode" type="text/javascript"></script>
+    <script src="https://unpkg.com/html5-qrcode" type="text/javascript">
+        function onScanSuccess(decodedText, decodedResult) {
+            // Handle on success condition with the decoded text or result.
+            console.log(`Scan result: ${decodedText}`, decodedResult);
+        }
+
+        var html5QrcodeScanner = new Html5QrcodeScanner(
+            "reader", {
+                fps: 10,
+                qrbox: 250
+            });
+        html5QrcodeScanner.render(onScanSuccess);
+    </script>
     <title>Inicia sesión</title>
 </head>
 
 <body>
     <h1>Página de inicio</h1>
     Camara<input type="file" name="image" accept="image/*" capture="environment">
+
+    <div style="width: 500px" id="reader"></div>
 </body>
 
 </html>
