f1b2461da73c51266564dc92845dc505033ac42c
jpabloroa <jpablonano@gmail.com>
2021-12-07 14:31:24 -0500

    Correcci├│n de errores

--- config/index.php
+++ config/index.php
@@ -1,41 +1,7 @@
 <?php
 
-$URL = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
-$inputUri = explode('/', $URL);
+//$urlPath = 
+require __DIR__ . "/config/bootstrap.php";
 
-for ($i = 0; $i < count($inputUri); $i++) {
-    echo "<h1>" . $inputUri[$i] . "</h1>";
-    /*
-    if ($inputUri[$i] == "index.php") {
-        $k = 0;
-        $j = $i + 1;
-        if ($j < count($inputUri)) {
-            for ($j; $j < count($inputUri); $j++) {
-                $parsedUri[$k] = $inputUri[$j];
-                $k++;
-            }
-        } else {
-
-            //
-            if (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on') {
-                $link = "https";
-            } else {
-                $link = "http";
-            }
-
-            // Here append the common URL characters.
-            $link .= "://";
-
-            // Append the host(domain name, ip) to the URL.
-            $link .= $_SERVER['HTTP_HOST'];
-
-            // Append the requested resource location to the URL
-            $link .= substr($URL, 0, -9) . "index.html";
-
-            //
-            header("location: $link");
-            exit;
-        }
-    }
-    */
-}
+header("location: " . $_SERVER["SERVER_NAME"] . "/proyectos/Application/config/");
+exit;
