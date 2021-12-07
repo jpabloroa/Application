a53114188aa4672aedd8b38096300867106319df
jpabloroa <jpablonano@gmail.com>
2021-12-07 14:52:12 -0500

    Correcci├│n de errores

--- config/index.php
+++ config/index.php
@@ -1,5 +1,21 @@
 <?php
-echo "location: " . __DIR__ . "/../../index.php";
-/*header("location: " . __DIR__ . "/../../index.php");
-exit;*/
+//
+if (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on') {
+    $link = "https";
+} else {
+    $link = "http";
+}
+
+// Here append the common URL characters.
+$link .= "://";
+
+// Append the host(domain name, ip) to the URL.
+$link .= $_SERVER['HTTP_HOST'];
+
+// Append the requested resource location to the URL
+$link .= substr($URL, 0, -9) . "index.html";
+
+//
+header("location: $link");
+exit;
 ?>
--- controller/index.php
+++ controller/index.php
@@ -1,4 +1,21 @@
 <?php
-header("location: " . __DIR__ . "/../index.php");
+//
+if (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on') {
+    $link = "https";
+} else {
+    $link = "http";
+}
+
+// Here append the common URL characters.
+$link .= "://";
+
+// Append the host(domain name, ip) to the URL.
+$link .= $_SERVER['HTTP_HOST'];
+
+// Append the requested resource location to the URL
+$link .= substr($URL, 0, -9) . "index.html";
+
+//
+header("location: $link");
 exit;
 ?>
