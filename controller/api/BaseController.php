87955e30140475c0edf84e21afa7c17eadd6bf93
jpabloroa <jpablonano@gmail.com>
2021-12-07 16:21:03 -0500

    Correcci├│n de errores

--- controller/api/BaseController.php
+++ controller/api/BaseController.php
@@ -6,7 +6,7 @@ class BaseController
      */
     public function __call($name, $arguments)
     {
-        $this->sendOutput(404, '', [], 'Recurso no encontrado');
+        $this->sendDefaultView();
     }
 
     /**
