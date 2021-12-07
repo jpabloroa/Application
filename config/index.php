534aa174ef9df75b8b3c0b6b9d9d1faea560f569
jpabloroa <jpablonano@gmail.com>
2021-12-07 14:55:00 -0500

    Correcci├│n de errores

--- config/index.php
+++ config/index.php
@@ -12,6 +12,9 @@ $link .= "://";
 // Append the host(domain name, ip) to the URL.
 $link .= $_SERVER['HTTP_HOST'];
 
+// Append the host(domain name, ip) to the URL.
+$link .= $_SERVER['REQUEST_URI'];
+
 // Append the requested resource location to the URL
 $link .= substr($URL, 0, -9) . "/index.html";
 
