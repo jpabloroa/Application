115f32c77b6253b4f32d7566e5d9ad6a5bd596d1
jpabloroa <jpablonano@gmail.com>
2021-12-07 16:25:04 -0500

    Correcci├│n de errores

--- controller/api/BaseController.php
+++ controller/api/BaseController.php
@@ -51,7 +51,7 @@ class BaseController
         $link .= $_SERVER['HTTP_HOST'];
 
         // Append the requested resource location to the URL
-        $link .= substr($_SERVER['REQUEST_URI'], 0, -9) . "/index.php";
+        $link .= substr($_SERVER['REQUEST_URI'], 0, - (strlen($_SERVER["SCRIPT_NAME"]) - 1)) . "/";
 
         //
         header("location: $link");
