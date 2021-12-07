8b8de5d640b9f976a0e4402276cb69e3c2b26d7f
jpabloroa <jpablonano@gmail.com>
2021-12-07 14:15:06 -0500

    Correcci├│n de errores

---
+++ controller/api/UserController.php
@@ -0,0 +1,31 @@
+<?php
+class UserController extends BaseController
+{
+
+    public function httpMethod($UrlPaths = [])
+    {
+        $requestMethod = $_SERVER["REQUEST_METHOD"];
+        $arrQueryStringParams = $this->getQueryStringParams();
+
+        switch (strtoupper($requestMethod)) {
+            case 'GET':
+                try {
+                    $userModel = new UserModel();
+
+                    $intLimit = 100;
+                    if (isset($arrQueryStringParams['limit']) && $arrQueryStringParams['limit']) {
+                        $intLimit = $arrQueryStringParams['limit'];
+                    }
+
+                    $arrUsers = $userModel->getUsers($intLimit);
+                    $this->sendOutput(200, $arrUsers, [], 'Se cargó(aron) ' . count($arrUsers) . ' cliente(s)');
+                } catch (Error $e) {
+                    $this->sendOutput(500, [], ['Internal Server Error - ' . $e->getMessage()], 'Detalles: ' . $e->getMessage());
+                }
+                break;
+            default:
+                $this->sendOutput(422, [], ['Unprocessable Entity'], 'Método ' . $requestMethod . ' no definido');
+                break;
+        }
+    }
+}
