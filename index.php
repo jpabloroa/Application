884f20182e7e8916541d416f710d0318e789d235
jpabloroa <jpablonano@gmail.com>
2021-12-07 19:42:33 -0500

    Correcci├│n de errores

--- index.php
+++ index.php
@@ -9,13 +9,14 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://unpkg.com/html5-qrcode"></script>
     <title>Inicia sesión</title>
 </head>
 
 <body>
     <h1>Página de inicio</h1>
     <div id="reader" style="width:600px;height:600px;"></div>
-    <script src="https://unpkg.com/html5-qrcode">
+    <script>
         const html5QrCode = new Html5Qrcode( /* element id */ "reader");
         // File based scanning
         const fileinput = document.getElementById('qr-input-file');
