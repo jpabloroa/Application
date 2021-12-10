6ece4edbc2cd37221c3d0e260b07bf10ae810332
jpabloroa <jpablonano@gmail.com>
2021-12-10 11:36:36 -0500

    Correcci├│n de errores

--- index.php
+++ index.php
@@ -18,7 +18,7 @@
     echo "primero que nada, primero que todo";
     ?>
     <h1>Página de inicio</h1>
-    <div id="reader" style="width:600px;height:600px;"></div>
+    <div id="reader" style="width:200px;height:200px;"></div>
     <input type="file" id="qr-input-file" accept="image/*">
     <!--<input type="file" id="qr-input-file" accept="image/*" capture>-->
     <script>
@@ -39,8 +39,7 @@
             html5QrCode.scanFile(imageFile, true)
                 .then(decodedText => {
                     // success, use decodedText
-                    alert("Escaneado !");
-                    console.log(decodedText);
+                    alert("Escaneado ! Resultado:" + decodedText);
                 })
                 .catch(err => {
                     // failure, handle it.
