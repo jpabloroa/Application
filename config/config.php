d57f2291616ddf4a35a2f2dea6a3254314b37fa0
jpabloroa <jpablonano@gmail.com>
2021-12-10 10:49:21 -0500

    Correcci├│n de errores

--- config/config.php
+++ config/config.php
@@ -2,4 +2,4 @@
 define("DB_HOST", "localhost");
 define("DB_USERNAME", "root");
 define("DB_PASSWORD", "Nad95037");
-define("DB_DATABASE_NAME", "jaggerbeats");
+define("DB_DATABASE_NAME", "JAGGERBEATS");
