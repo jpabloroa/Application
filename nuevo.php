9fed6e53400813e6fbc214d96f528b4d3d4a401c
jpabloroa <jpablonano@gmail.com>
2021-12-11 02:45:49 -0500

    Correcci├│n de errores

--- nuevo.php
+++ nuevo.php
@@ -2,52 +2,83 @@
 
 require __DIR__ . "/config/bootstrap.php";
 
-$URL = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
-$inputUri = explode('/', $URL);
+//
+session_start();
 
-for ($i = 0; $i < count($inputUri); $i++) {
-    if ($inputUri[$i] == "index.php") {
-        $k = 0;
-        $j = $i + 1;
-        if ($j < count($inputUri)) {
-            for ($j; $j < count($inputUri); $j++) {
-                $parsedUri[$k] = $inputUri[$j];
-                $k++;
+//
+if (isset($_SESSION["userControl"])) {
+
+    //
+    $UserCredentials = $_SESSION["userControl"];
+
+    if (isset($_SESSION["userControl"])) {
+
+        //
+        $URL = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+        $inputUri = explode('/', $URL);
+
+        for ($i = 0; $i < count($inputUri); $i++) {
+            if ($inputUri[$i] == "nuevo.php") {
+                $k = 0;
+                $j = $i + 1;
+
+                if ($j < count($inputUri)) {
+                    for ($j; $j < count($inputUri); $j++) {
+                        $parsedUri[$k] = $inputUri[$j];
+                        $k++;
+                    }
+                } else {
+
+                    require PROJECT_ROOT_PATH . "/controller/api/UserController.php";
+
+                    //
+                    $objFeedController = new UserController();
+                    $objFeedController->sendResponse(404, [], ["Not Found"], "");
+                }
+            }
+        }
+
+        require PROJECT_ROOT_PATH . "/controller/api/UserController.php";
+        $objFeedController = new UserController();
+
+        if (isset($parsedUri[0]) && $parsedUri[0] != null) {
+
+            //
+            if ($parsedUri[0] == "finalize") {
+
+                //
+                $objFeedController->closeSession();
+            } else {
+
+                //
+                $objFeedController->httpMethod($parsedUri);
             }
         } else {
 
             //
-            if (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on') {
-                $link = "https";
-            } else {
-                $link = "http";
-            }
-
-            // Here append the common URL characters.
-            $link .= "://";
-
-            // Append the host(domain name, ip) to the URL.
-            $link .= $_SERVER['HTTP_HOST'];
-
-            // Append the requested resource location to the URL
-            $link .= substr($URL, 0, -9) . "index.php";
-
-            //
-            header("location: $link");
-            exit;
+            $objFeedController->sendResponse(404, [], ["Not Found"], "");
         }
     }
-}
-
-require PROJECT_ROOT_PATH . "/controller/api/UserController.php";
-
-if (isset($parsedUri[0]) && $parsedUri[0] == "root") {
-    $objFeedController = new UserController();
-    $objFeedController->{$parsedUri[0]}();
 } else {
-    if (isset($parsedUri[1])) {     
-        $objFeedController = new UserController();
-        $objFeedController->httpMethod($parsedUri);
+
+    require PROJECT_ROOT_PATH . "/controller/api/UserController.php";
+
+    //
+    parse_str($_SERVER['QUERY_STRING'], $urlQuery);
+    $objFeedController = new UserController();
+
+    //
+    if (isset($urlQuery["key"]) && $urlQuery["key"]) {
+
+        //
+        $queryString = $urlQuery["key"];
+
+        //
+        $arrayCredentials = explode(":", $queryString);
+        $UserCredentials["user"] = ($arrayCredentials[0] == null || $arrayCredentials[0] == "") ? null : $arrayCredentials[0];
+        $UserCredentials["password"] = ($arrayCredentials[1] == null || $arrayCredentials[1] == "") ? null : $arrayCredentials[1];
+        $objFeedController->validateCredentials($UserCredentials);
     } else {
+        $objFeedController->sendResponse(400, [], ["Bad Request"], "No se ha iniciado sesión");
     }
 }
