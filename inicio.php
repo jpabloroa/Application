1f1d3a966d398da6b8d6d3caf4ffa122391789c9
jpabloroa <jpablonano@gmail.com>
2021-12-12 21:08:20 -0500

    Correcci├│n de errores

---
+++ inicio.php
@@ -0,0 +1,19 @@
+<?php
+
+$request = $_SERVER['REQUEST_URI'];
+
+switch ($request) {
+    case '/' :
+        require __DIR__ . '/views/index.php';
+        break;
+    case '' :
+        require __DIR__ . '/views/index.php';
+        break;
+    case '/hallo' :
+        require __DIR__ . '/administrador.php/clientes/';
+        break;
+    default:
+        http_response_code(404);
+        require __DIR__ . '/views/404.php';
+        break;
+}
