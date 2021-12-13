1b968a042ed7468ccd4906658eceaf1982627ca1
jpabloroa <jpablonano@gmail.com>
2021-12-12 22:57:27 -0500

    Correcci├│n de errores

--- inicio.php
+++ inicio.php
@@ -4,9 +4,9 @@
 // We could put this in a different file and include it in order to separate
 // logic and configuration.
 $routes = [
-    "/" => "administrador/clientes/",
-    "/hello" => "perico.php",
-    "error_404" => "pages/error_page.php?error=404"
+    "/" => "/administrador/clientes/",
+    "/hello" => "/perico.php",
+    "error_404" => "/pages/error_page.php?error=404"
 ];
 
 // This is our router.
@@ -15,32 +15,13 @@ function router($routes)
     // Iterate through a given list of routes.
     foreach ($routes as $path => $content) {
         if ($path == "/" . $_REQUEST["view"]) {
-            // If the path matches, display its contents and stop the router.
-            //
-            if (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on') {
-                $link = "https";
-            } else {
-                $link = "http";
-            }
-
-            // Here append the common URL characters.
-            $link .= "://";
-
-            // Append the host(domain name, ip) to the URL.
-            $link .= $_SERVER['HTTP_HOST'];
-
-            // Append the requested resource location to the URL 
-            $link .= $_SERVER['REQUEST_URI'];
-            echo "Redireccionando a $link" . $content;
-
-            //
-            //header("location: $link");
+            include(__DIR__ . $content);
             exit;
         }
     }
 
     // This can only be reached if none of the routes matched the path.
-    include($routes["error_404"]);
+    include(__DIR__ . $routes["error_404"]);
 }
 
 // Execute the router with our list of routes.
