7f02295d4ba5a3bd9fab60aeb52d7ce4cfc15c6b
jpabloroa <jpablonano@gmail.com>
2021-12-12 21:21:00 -0500

    Correcci├│n de errores

--- inicio.php
+++ inicio.php
@@ -1,19 +1,49 @@
-<?php
+<?php 
 
-$request = $_SERVER['REQUEST_URI'];
 
-switch ($request) {
-    case '/' :
-        require __DIR__ . '/views/index.php';
-        break;
-    case '' :
-        require __DIR__ . '/views/index.php';
-        break;
-    case '/hallo' :
-        require __DIR__ . '/administrador.php/clientes/';
-        break;
-    default:
-        http_response_code(404);
-        require __DIR__ . '/views/404.php';
-        break;
-}
+
+include "route.php";
+
+
+
+
+
+/**
+ * -----------------------------------------------
+ * PHP Route Things
+ * -----------------------------------------------
+ */
+
+//define your route. This is main page route. for example www.example.com
+Route::add('/', function(){
+
+    //define which page you want to display while user hit main page. 
+    include('myindex.php');
+});
+
+
+// route for www.example.com/join
+Route::add('/join', function(){
+    include('join.php');
+});
+
+Route::add('/login', function(){
+    include('login.php');
+});
+
+Route::add('/forget', function(){
+    include('forget.php');
+});
+
+
+
+Route::add('/logout', function(){
+    include('logout.php');
+});
+
+
+
+
+
+//method for execution routes    
+Route::submit();
